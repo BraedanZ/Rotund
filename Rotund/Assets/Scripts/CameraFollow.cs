7dfb889b6ca4c89cc82c9dfb5733e771fcca3193
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2024-11-06 14:44:07 -0500

    Test world created

---
+++ Rotund/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+
+    public Vector3 offset;
+
+    public Camera cam;
+
+    void Start () {
+        cam = GetComponent<Camera>();
+        SnapCamera();
+    }
+
+    void FixedUpdate () {
+        SmoothCameraFollow();
+    }
+
+    public void SnapCamera () {
+        transform.position = target.position + offset;
+    }
+
+    private void SmoothCameraFollow() {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
