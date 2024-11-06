53393422d84c5b07d2bd682fcfc8dd8615c33415
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2024-11-06 15:21:01 -0500

    small functions

--- Rotund/Assets/Scripts/Player.cs
+++ Rotund/Assets/Scripts/Player.cs
@@ -7,12 +7,20 @@ public class Player : MonoBehaviour
     Player player;
     Rigidbody2D rigidBody;
 
+    private new CameraFollow camera;
+
     float input;
     public float speed;
+
+    private bool small = false;
+    private Vector3 scaleChange;
     void Start()
     {
         player = this;
         rigidBody = GetComponent<Rigidbody2D>();
+        scaleChange = new Vector3(0.2f, 0.2f, 0.2f);
+
+        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFollow>();
     }
 
     void FixedUpdate()
@@ -25,5 +33,16 @@ public class Player : MonoBehaviour
         input = Input.GetAxisRaw("Horizontal");
         rigidBody.AddForce(new Vector2(1, 0) * input * speed);
     //     rigidBody.AddTorque(-(input), ForceMode2D.Force);
+    }
+
+    public void Small() {
+        if (small) {
+            return;
+        } else {
+            player.transform.localScale -= scaleChange;
+            rigidBody.mass = 0.35f;
+            camera.Small();
+            small = true;
+        }
     }
 }
