af178f67c7cea5350a2d574cd3bac78437f8d52a
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2026-06-11 02:42:58 -0400

    swipe restarts game

--- Rotund/Assets/Scripts/Player.cs
+++ Rotund/Assets/Scripts/Player.cs
@@ -45,6 +45,11 @@ public class Player : MonoBehaviour
     // public float extraBigGravity = 3f;
     private bool finished = false;
 
+    private Vector2 startSwipePosition;
+    private Vector2 endSwipePosition;
+
+    public float swipeThreshold = 75f;
+
     void Start()
     {
         player = this;
@@ -110,18 +115,26 @@ public class Player : MonoBehaviour
     }
 
     private void DetectInput() {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0)) {
+        if (Input.GetKeyDown(KeyCode.Space)) {
             ChangeSize();
         }
-        // if (Input.GetKeyDown(KeyCode.Mouse0)) {
-        //     Vector3 mousePosition = Input.mousePosition;
-        //     if (mousePosition.x > Screen.width / 2)
-        //     {
-        //         ChangeSize();
-        //     } else if (mousePosition.y > Screen.height / 2) {
-        //         gameMaster.Restart();
-        //     }
-        // }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            startSwipePosition = Input.mousePosition;
+        } else if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            endSwipePosition = Input.mousePosition;
+            CheckSwipe();
+        }
+    }
+
+    private void CheckSwipe()
+    {
+        Vector2 swipeVector = endSwipePosition - startSwipePosition;
+        if (swipeVector.magnitude > swipeThreshold)
+        {
+            gameMaster.Restart();
+        }
     }
 
     private void ChangeSize() {
